a7321fa5e9e684e6f1131a524dfed7fab3516cdf
DMSN <73945997+domants@users.noreply.github.com>
2023-05-07 23:13:22 +0800

    Update CreateTaskPlugin.cs

--- CreateTaskPlugin.cs
+++ CreateTaskPlugin.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Sdk;
-
+using System;
+using System.ServiceModel;
 namespace DMSNPlugins
 {
     public class CreateTaskPlugin : IPlugin
@@ -11,30 +10,48 @@ namespace DMSNPlugins
             // Get the execution context
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
 
-            //Extract tracing service
+            //Extract tracing service for debugging
             ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
 
-            // Check if the plugin is triggered by the create message and the target is an account entity
-            if (context.MessageName.ToLower() == "create" && context.PrimaryEntityName.ToLower() == "account")
-            {
-                // Get context of account record
-                Entity account = (Entity)context.InputParameters["Target"];
+            // Get the organization service
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            try
+            { // Check if the plugin is triggered by the create message and the target is an account entity
+                if (context.MessageName.ToLower() == "create" && context.PrimaryEntityName.ToLower() == "account")
+                {
+                    // Get context of account record / Primary entity
+                    Entity entity = (Entity)context.InputParameters["Target"];
+                    tracingService.Trace($"Target Entity: {entity.LogicalName}");
 
-                // Create a new task note related to the account
-                Entity task = new Entity("task");
-                task.Attributes["subject"] = "New Account Created";
-                task.Attributes["description"] = "A new account with name '" + account.Attributes["name"] + "' has been created.";
-                //set up lookup value
-                task.Attributes["regardingobjectid"] = new EntityReference("account", account.Id);
-                
-                // Get the organization service
-                IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+                    // Create a new task note related to the account
+                    Entity task = new Entity("task");
+                    task.Attributes["subject"] = "New Account Created";
+                    //task.Attributes["description"] = $"A new account with name {entity.Attributes["name"]} has been created.";
+                    task.Attributes.Add("description", $"A new account with name {entity.Attributes["name"]} has been created.");
 
-                // Create the task note record
-                service.Create(task);
-                tracingService.Trace("Task note successfully created!");
+                    //set up lookup value
+                    //task.Attributes["regardingobjectid"] = new EntityReference($"{entity.LogicalName}", entity.Id);
+                    //task.Attributes["regardingobjectid"] = entity.ToEntityReference();
+                    task.Attributes.Add("regardingobjectid", entity.ToEntityReference());
+
+                    // Create the task note record
+                    service.Create(task);
+                    tracingService.Trace("Task note successfully created!");
+                }
             }
+
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                throw new InvalidPluginExecutionException("An error occured", ex);
+            }
+
+            catch (Exception ex)
+            {
+                tracingService.Trace("Invalid Plugin {0}", ex.ToString());
+                throw;
+            }
+
         }
     }
 }
