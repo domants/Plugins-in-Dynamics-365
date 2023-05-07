d1bc7f51007bcfd5224a309e12966795dbe2e2e8
DMSN <73945997+domants@users.noreply.github.com>
2023-05-08 00:29:19 +0800

    Update DuplicateCheckEmail.cs

--- DuplicateCheckEmail.cs
+++ DuplicateCheckEmail.cs
@@ -17,44 +17,53 @@ namespace DMSNPlugins
             Entity entity = (Entity)context.InputParameters["Target"];
 
 
-            try
+
+            // Check if the plugin is triggered by the create or update message and the target is an account entity
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
             {
-                // Check if the plugin is triggered by the create or update message and the target is an account entity
                 if (context.MessageName.ToLower() == "create" || context.MessageName.ToLower() == "update")
                 {
-                    // Get the organization service
-                    IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-                    IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
 
-                    var email = string.Empty;
-                    //check if attribute has value
-                    if (entity.Attributes.Contains("emailaddress1"))
-                        email = entity.Attributes["emailaddress1"].ToString();
-
-                    // Retrieve all records in the contact entity where the emailaddress is equal to email input"
-                    QueryExpression query = new QueryExpression("contact");
-                    query.ColumnSet = new ColumnSet("emailaddress1");
-                    query.Criteria.AddCondition("emailaddress1", ConditionOperator.Equal, email);
-                    EntityCollection collection = service.RetrieveMultiple(query);
-
-                    // Check if the email exist 
-                    if (collection.Entities.Count > 0)
+                    try
                     {
-                        throw new InvalidPluginExecutionException("Contact with email already exist!");
+                        // Get the organization service
+                        IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                        IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                        var email = string.Empty;
+                        //check if attribute has value
+                        if (entity.Attributes.Contains("emailaddress1"))
+                            email = entity.Attributes["emailaddress1"].ToString();
+
+                        // Retrieve all records in the contact entity where the emailaddress is equal to email input"
+                        QueryExpression query = new QueryExpression("contact");
+                        query.ColumnSet = new ColumnSet("emailaddress1");
+                        query.Criteria.AddCondition("emailaddress1", ConditionOperator.Equal, email);
+                        EntityCollection collection = service.RetrieveMultiple(query);
+
+                        // Check if the email exist 
+                        if (collection.Entities.Count > 0)
+                            throw new InvalidPluginExecutionException("Contact with email already exist!");
+
                     }
+
+
+
+
+                    catch (FaultException<OrganizationServiceFault> ex)
+                    {
+                        throw new InvalidPluginExecutionException("An error occured", ex);
+                    }
+
+                    catch (Exception ex)
+                    {
+                        tracingService.Trace("Invalid Plugin {0}", ex.ToString());
+                        throw;
+                    }
+
                 }
             }
 
-            catch (FaultException<OrganizationServiceFault> ex)
-            {
-                throw new InvalidPluginExecutionException("An error occured", ex);
-            }
-
-            catch (Exception ex)
-            {
-                tracingService.Trace("Invalid Plugin {0}", ex.ToString());
-                throw;
-            }
 
         }
     }
